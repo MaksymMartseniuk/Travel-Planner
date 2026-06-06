6328fad2efcd80fc09ebd5b516376fde75a1f83d
MaksymMartseniuk <marcenukm995@gmail.com>
2026-06-06 16:47:39 +0300

    feat: create Project and Place models with relationships

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    """Model representing a travel project."""
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def is_completed(self):
+        places = self.places.all()
+        if not places.exists():
+            return False
+        return all(place.is_visited for place in places)
+            
+    
+class Place(models.Model):
+    """Model representing a place to visit."""
+    project = models.ForeignKey(Project, related_name='places', on_delete=models.CASCADE)
+    
+    external_id = models.CharField(max_length=255, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    is_visited = models.BooleanField(default=False)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.external_id} in {self.project.name} ({'Visited' if self.is_visited else 'Not Visited'})"
--- travel_planner_api/settings.py
+++ travel_planner_api/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_spectacular',
-    
+
     'planner',
 ]
 
@@ -81,8 +81,16 @@ WSGI_APPLICATION = 'travel_planner_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
